7413fc597183a5e950dcbfaf7989568873f817ca
lindabrugmans <99263515+lindabrugmans@users.noreply.github.com>
2024-09-19 15:36:52 +0200

    Update Container.tsx

--- portfolio/src/shared/Container/Container.tsx
+++ portfolio/src/shared/Container/Container.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import styles from './Container.module.scss';
 
-interface ContainerProps{
+interface IContainer{
 
   children: React.ReactNode;
   className?: string;
 }
 
-export default function Container({ children, className }: ContainerProps) {
+export default function Container({ children, className }: IContainer) {
   return (
 
     <div className={`${styles.container} ${className}`}>{children}</div>
